e5dff9ed690a63ad5e2e46bc855fe9060dbab876
Charles Xu <81684416+Char15Xu@users.noreply.github.com>
2022-10-30 19:39:14 -0700

    Charles Xu single choice update

--- Frontend Code/src/QuizView.tsx
+++ Frontend Code/src/QuizView.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import MarkdownTextView from './Components/MarkdownTextView';
 import { Question, QuizMetadata } from './Data Structures';
 import './QuizView.css'
+import './quizdesign.json'
 
 interface QuizViewProps {
     metadata: QuizMetadata
@@ -28,17 +29,38 @@ class QuizView extends React.Component<QuizViewProps, QuizViewState> {
 
         // Here is an example of how you can insert a variable number of elements into a page
         let questions: JSX.Element[] = []
-
-        for (let i = 0; i < 10; i++) {
-            questions.push(<div style={{padding: '10px', margin: '10px 0', border: '1px dashed lightgray'}}>{`Example question ${i+1}`}</div>)
-        }
-
+    
         return (
-            <div className='quiz-container'>
+            <div className='quiz_container'>
                 <h2 className='quiz-title'><MarkdownTextView rawText={this.props.metadata.quiz_title} /></h2>
                 <MarkdownTextView rawText={this.props.metadata.quiz_subtitle || undefined} />
                 
-                {questions}
+                <div className='question_number'>
+                    <span><MarkdownTextView rawText='**Question 1**' /> </span>
+                </div>
+
+                <div className='single_choice'>
+                    <h3 className='question-title'>Which of the following choices is the capital of California?</h3>
+                    <p className='single-choice-indicator'>Select The Best Choice From The Following Options</p>
+                        <div className = "question_content">
+
+                            <input type="radio" name="langs" className = 'single-box' value='San Francisco' id='single-check-1'/> 
+                            <label className='single' htmlFor='single-check-1'><span>San Francisco</span></label>
+                        
+                            <input type="radio" name="langs" className = 'single-box' value='Los Angeles' id='single-check-2'/>
+                            <label className='single' htmlFor='single-check-2'><span>Los Angeles</span></label>
+                            
+                            <input type="radio" name="langs" className = 'single-box' value='Las Vegas' id='single-check-3'/>
+                            <label className='single' htmlFor='single-check-3'><span>Las Vegas</span></label>
+
+                            <input type="radio" name="langs" className = 'single-box' value='Sacramento' id='single-check-4'/> 
+                            <label className='single' htmlFor='single-check-4'><span>Sacramento</span></label>
+
+                        </div>
+                    <button className="button-singlechoice-submit">Submit</button>
+                    
+                    {questions}
+                </div>
             </div>
 
         )
